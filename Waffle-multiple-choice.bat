acbe73d86fe80c76586741d7c4a330f75cf3a157
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2022-11-16 16:05:03 +0000

    Create Waffle-multiple-choice.bat

---
+++ Waffle-multiple-choice.bat
@@ -0,0 +1,23 @@
+@echo on
+echo hello
+echo do you like food yes or no
+if yes:
+goto a
+if no:
+echo continue then
+goto b
+:a
+echo carry on becuase its good infact do 10 pushups
+goto c
+:b
+sorry to hear that go and follow your passion =)
+:c
+echo do you like lego aye or non
+if aye:
+goto d
+if non:
+goto e
+:d
+echo ok i like lego aswell but this file is finished
+:e
+echo plz buy some u will love it =)  this file is finished
